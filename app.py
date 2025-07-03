755b0abc9fdcfaa290925b677fe2a50f7c821f38
John Jairo Castro <168293591+johnjacama24@users.noreply.github.com>
2025-07-03 17:39:44 -0500

    Update app.py

--- app.py
+++ app.py
@@ -28,7 +28,7 @@ inv_estado_civil = {v: k for k, v in dicc_estado_civil.items()}
 # ------------------------------
 # Interfaz de usuario
 # ------------------------------
-st.title("🧠 Predicción del Estado del Aprendiz")
+st.title("🧠 Predicción del Estado del Aprendiz ANIMACIÓN DIGITAL")
 st.markdown("Seleccione las opciones correspondientes y presione el botón para predecir.")
 
 # Campos de entrada
